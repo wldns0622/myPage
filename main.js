bf3d2dc7a0079d9b638d905972c51618c276213e
wldns0622 <wldns12378@gmail.com>
2021-03-02 00:59:59 +0900

    JS: Navbar menu click scroll

--- main.js
+++ main.js
@@ -11,3 +11,63 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 })
+
+
+// Handle scrolling when tapping on the navbar menu
+
+// 1. 메뉴를 클릭시 이벤트를 생성한다
+// 2. 해당 메뉴 textContent가 변수와 같을 때
+// 3. 해당 변수의 위치로 스크롤링
+
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (!link) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+})
+
+/*
+const home = document.querySelector('#home').offsetTop;
+const about = document.querySelector('#about').offsetTop;
+const skills = document.querySelector('#skills').offsetTop;
+const work = document.querySelector('#work').offsetTop;
+const testimonials = document.querySelector('#testimonials').offsetTop;
+const contact = document.querySelector('#contact').offsetTop;
+const menuItems = document.querySelectorAll('.navbar__menu__item');
+
+for (let menu of menuItems) {
+  menu.addEventListener('click', (e) => {
+    switch (e.target.textContent.toLowerCase()) {
+      case 'home':
+        window.scrollTo({ top: home, behavior: 'smooth' });
+        break;
+      case 'about':
+        window.scrollTo({ top: about, behavior: 'smooth' });
+        break;
+      case 'skills':
+        window.scrollTo({ top: skills, behavior: 'smooth' });
+        break;
+      case 'my work':
+        window.scrollTo({ top: work, behavior: 'smooth' });
+        break;
+      case 'testimonial':
+        window.scrollTo({ top: testimonials, behavior: 'smooth' });
+        break;
+      case 'contact':
+        window.scrollTo({ top: contact, behavior: 'smooth' });
+        break;
+    }
+  });
+}
+*/
+
+const contactBtn = document.querySelector('.home__contact');
+contactBtn.addEventListener('click', (e) => {
+  // e.defaultPrevented();
+  window.scrollTo({ top: contact, behavior: 'smooth' });
+});
